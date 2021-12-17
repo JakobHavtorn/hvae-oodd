0a24c9f18d6c048d9fc9faac032698bd405cbbda
Janek <jan.piotr.latko@gmail.com>
2021-12-17 13:14:55 +0100

    cifar groups

---
+++ scripts/run_experiments7.sh
@@ -0,0 +1,31 @@
+GPU=7
+LIKELIHOOD="DiscretizedLogisticMixLikelihoodConv2d"
+VAL_DATA="scripts/configs/val_datasets/cifar_topics.json"
+
+
+CUDA_VISIBLE_DEVICES=$GPU python scripts/dvae_run.py \
+                               --seed 1 \
+                               --train_datasets \
+                               '{
+                                   "CIFAR100DequantizedThings": {"dynamic": true, "split": "train"}
+                               }' \
+                               --val_datasets $VAL_DATA \
+                               --likelihood $LIKELIHOOD \
+                               --config_deterministic scripts/configs/default_model/config_deterministic.json \
+                               --config_stochastic scripts/configs/default_model/config_stochastic.json
+
+
+CUDA_VISIBLE_DEVICES=$GPU python scripts/dvae_run.py \
+                               --seed 1 \
+                               --train_datasets \
+                               '{
+                                   "CIFAR100DequantizedRest": {"dynamic": true, "split": "train"}
+                               }' \
+                               --val_datasets $VAL_DATA \
+                               --likelihood $LIKELIHOOD \
+                               --config_deterministic scripts/configs/default_model/config_deterministic.json \
+                               --config_stochastic scripts/configs/default_model/config_stochastic.json
+
+
+
+
